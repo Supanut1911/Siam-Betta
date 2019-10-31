87c9582714b209d1310dca883b68f5cf6be674a3
supanut1911 <ynotmela@gmail.com>
2019-10-31 13:09:42 +0700

    Initial Commit

---
+++ Siam Betta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Siam Betta
+//
+//  Created by Supanut Laddayam on 31/10/2562 BE.
+//  Copyright © 2562 Supanut Laddayam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
